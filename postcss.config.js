9768e9b297287bb0c3dc1b95d3539ed03ea6e054
tienv2i <tienv2i@gmail.com>
2022-08-28 10:48:00 +0700

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,9 @@ const purgecssWordpress = require('purgecss-with-wordpress');
 module.exports = {
     plugins: [
       require('autoprefixer'),
-      // purgecss({
-      //   content: ['./**/*.html','./**/*.php','./**/*.js'],
-      //   safelist: purgecssWordpress.safelist
-      // })
+      purgecss({
+        content: ['./**/*.html','./**/*.php','./**/*.js'],
+        safelist: purgecssWordpress.safelist
+      })
     ]
   }
